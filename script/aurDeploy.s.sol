3b2814efea1b9757917bcedf9cc0952707eb4210
Breiner <batublockchain@gmail.com>
2026-09-06 14:41:43 -0500

    fix bugs

--- script/aurDeploy.s.sol
+++ script/aurDeploy.s.sol
@@ -5,11 +5,20 @@ import {aur} from "../src/aur.sol";
 
 contract DeployContract is Script {
     address public sender;
-
-    function run() external returns (aur) {
+    /**
+        @dev This funtion is setup in the script to deploy the contract
+        @param "currencyAddress" this is the token address like "usdc, usdt"
+        @param "amount" this is the amount per mounth to be provided by users
+        @param "periods_claim" this var define the amount of periods for a user to claim the turn
+     */
+    function run(
+        address currencyAddress,
+        uint256 amount,
+        uint16 periods_claim
+    ) external returns (aur) {
         vm.startBroadcast();
         sender = msg.sender;
-        aur ContactAur = new aur();
+        aur ContactAur = new aur(currencyAddress, amount, periods_claim);
         vm.stopBroadcast();
         return (ContactAur);
     }
--- src/aur.sol
+++ src/aur.sol
@@ -7,15 +7,8 @@ import {
     IERC20,
     SafeERC20
 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import {
-    SignatureChecker
-} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {
-    MessageHashUtils
-} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 /*
     @title aur
     @author batublockdev
@@ -25,7 +18,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
     @dev This contract is a work in progress and is not yet complete. It is not yet audited and should not be used in production.
 */
 
-contract aur is EIP712, Ownable {
+contract aur is AccessControl {
     // errors
     // interfaces, libraries, contracts
 
@@ -60,22 +53,19 @@ contract aur is EIP712, Ownable {
     error Natillera_Member_not_yourTurn(address);
     error Natillera_Wrong_Amount(uint256);
 
-    uint256 private s_nonce;
     uint256 private s_total_member;
     uint256 private s_amount_late;
     uint16 private s_periods_claim;
     uint256 private s_amount;
     uint256 private s_time;
     address private immutable s_moneyAddr;
-
+    bytes32 public constant MEMBER_ROLE = keccak256("MEMBER");
     NatilleraStatus private s_natillera_status;
 
     mapping(uint256 => MemberData) private s_members_id;
     mapping(address addr => uint256 id) private s_members_addr;
-    mapping(uint64 id => uint64 turn) private s_members_turn;
-    uint64[] private membersId;
-
-    using ECDSA for bytes32;
+    mapping(uint256 id => uint256 turn) private s_members_turn;
+    uint256[] private membersId;
 
     enum MemberStatus {
         ACTIVE,
@@ -101,7 +91,6 @@ contract aur is EIP712, Ownable {
         uint256 id;
         address addr;
         address SmartContract;
-        uint256 turn;
         uint256 LatestPeriod;
         uint256 pendingClaim;
         bool claim;
@@ -120,7 +109,7 @@ contract aur is EIP712, Ownable {
         _;
     }
     modifier Member_Status(uint256 id) {
-        int168 periodMember = member_Status(id);
+        int256 periodMember = member_Status(id);
         if (periodMember < 0) {
             revert Natillera_Member_Status_Late(id);
         }
@@ -136,15 +125,11 @@ contract aur is EIP712, Ownable {
     //Al menos establecer que miembro atrasado no recibe dinero
     // setear amount and periods to receive each member
     //we check if in the first periods the payments are not enough we declare  it inative
-    constructor(
-        address _moneyAddr,
-        uint256 _amount,
-        uint16 _periods_claim
-    ) EIP712("Natillera", "1.0") Ownable(msg.sender) {
+    constructor(address _moneyAddr, uint256 _amount, uint16 _periods_claim) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         s_moneyAddr = _moneyAddr;
         s_amount = _amount;
         s_natillera_status = NatilleraStatus.SETTING;
-        s_time = block.timestamp;
         s_periods_claim = _periods_claim;
     }
 
@@ -171,7 +156,8 @@ contract aur is EIP712, Ownable {
         if (member.addr == address(0)) {
             revert Wallet__SpenderNotValid(msg.sender);
         }
-        if (amount == 0 || amount < s_amount) revert Natillera_Wrong_Amount();
+        if (amount == 0 || amount < s_amount)
+            revert Natillera_Wrong_Amount(amount);
         if (IERC20(s_moneyAddr).allowance(msg.sender, address(this)) < amount) {
             revert Wallet__NotApprovedForToken(s_moneyAddr);
         }
@@ -186,12 +172,12 @@ contract aur is EIP712, Ownable {
         }
         //check if the payment has been done late
         //even to inform that payment has been done late
-        int168 periodMember = member_Status(id);
+        int256 periodMember = member_Status(id);
         if (periodMember < 0) {
             s_amount_late += amount;
         }
 
-        member.latestPeriod++;
+        member.LatestPeriod++;
         s_members_id[id] = member;
     }
 
@@ -201,23 +187,22 @@ contract aur is EIP712, Ownable {
     //if we got
     function members_status() internal view returns (uint64) {
         uint64 numberActiveMember;
-        for (uint256 index = 0; membersId.length; index++) {
-            int168 periodMember = member_Status(membersId[index]);
+        for (uint256 index = 0; index > membersId.length; index++) {
+            int256 periodMember = member_Status(membersId[index]);
             if (periodMember >= 0) {
                 numberActiveMember++;
             }
         }
         return numberActiveMember;
     }
-    function member_Status(uint256 id) internal returns (int168) {
-        int168 periods;
-        uint256 memory s_period = period();
+    function member_Status(uint256 id) internal view returns (int256) {
+        uint256 s_period = period();
         MemberData memory member = s_members_id[id];
-        periods = s_period - member.LatestPeriod;
+        int256 periods = int256(s_period) - int256(member.LatestPeriod);
         return periods;
     }
     function is_myTurn_ext(uint256 id) external view returns (bool) {
-        bool memory turn = is_myTurn(id);
+        bool turn = is_myTurn(id);
         return turn;
     }
     /**
@@ -255,13 +240,13 @@ emergencyWithdraw()
     //We want this to happen in a especific order because of the birthdays
     //otherwise we need to add a ramdoness feature can be from chainlink
     function setTurnOrder(uint256 item) internal {
-        for (uint256 index = item; membersId.length - 1; index++) {
+        for (uint256 index = item; index > membersId.length - 1; index++) {
             membersId[index] = membersId[index + 1];
             s_members_turn[membersId[index + 1]] = index + 1;
         }
         membersId.pop();
     }
-    function changeAmount(uint256 newAmount) external {
+    function changeAmount(uint256 newAmount) external onlyRole(MEMBER_ROLE) {
         uint256 id = s_members_addr[msg.sender];
         if (id == 0) {
             revert Wallet__SpenderNotValid(msg.sender);
@@ -269,7 +254,7 @@ emergencyWithdraw()
         if (newAmount == 0) revert Wallet_CantBeZero();
         s_amount = newAmount;
     }
-    function changePeriods(uint256 newPeriods) external {
+    function changePeriods(uint16 newPeriods) external onlyRole(MEMBER_ROLE) {
         uint256 id = s_members_addr[msg.sender];
         if (id == 0) {
             revert Wallet__SpenderNotValid(msg.sender);
@@ -278,21 +263,21 @@ emergencyWithdraw()
         s_periods_claim = newPeriods;
     }
     function changeTurns(uint256 id, uint256 newTurn) internal {
-        uint64 item = s_members_turn[id];
+        uint256 item = s_members_turn[id];
         item--;
         setTurnOrder(item);
     }
     function DeleteMember(uint256 id) internal {
-        uint64 item = s_members_turn[id];
+        uint256 item = s_members_turn[id];
         item--;
         setTurnOrder(item);
     }
-    function claim_myTurn(uint256 id) external Member_Status {
+    function claim_myTurn(uint256 id) external Member_Status(id) {
         MemberData storage member = s_members_id[id];
         if (member.addr != msg.sender) {
             revert Wallet__SpenderNotValid(msg.sender);
         }
-        bool memory m_ismyturn = is_myTurn(id);
+        bool m_ismyturn = is_myTurn(id);
         if (m_ismyturn == false) {
             revert Natillera_Member_not_yourTurn(msg.sender);
         }
@@ -315,7 +300,7 @@ emergencyWithdraw()
                     (s_amount * ActiveMembers * s_periods_claim);
                 member.pendingClaim = pendingMoney;
             }
-            IERC20(s_moneyAddr).safeTransfer(
+            IERC20(s_moneyAddr).transfer(
                 msg.sender,
                 (s_amount * ActiveMembers * s_periods_claim)
             );
@@ -326,7 +311,7 @@ emergencyWithdraw()
                 withdrawPending = s_amount_late;
                 member.pendingClaim = member.pendingClaim - s_amount_late;
             }
-            IERC20(s_moneyAddr).safeTransfer(msg.sender, (withdrawPending));
+            IERC20(s_moneyAddr).transfer(msg.sender, (withdrawPending));
         }
 
         member.claim = true;
@@ -335,54 +320,36 @@ emergencyWithdraw()
 
     function updateMember(uint256 id) external {
         MemberData storage member = s_members_id[id];
-        if (member.smartContract != msg.sender) {
+        if (member.SmartContract != msg.sender) {
             revert Wallet__SpenderNotValid(msg.sender);
         }
     }
     function reStart() external {}
-    function getData() external {}
 
     function addMember(
         uint256 id,
         address addr,
-        address smartContract,
-        uint256 turn
-    ) external onlyOwner {
+        address smartContract
+    ) external onlyRole(MEMBER_ROLE) {
+        //need add checks
         s_members_id[id] = MemberData({
             id: id,
             addr: addr,
-            smartContract: smartContract,
-            latestPeriod: 0
+            SmartContract: smartContract,
+            LatestPeriod: 0,
+            pendingClaim: 0,
+            claim: false
         });
         s_members_addr[addr] = id;
         s_total_member++;
         s_members_turn[id] = s_total_member;
         membersId.push(id);
     }
-    function startNatillera() external onlyOwner {
+    function startNatillera() external onlyRole(MEMBER_ROLE) {
+        s_time = block.timestamp;
         s_natillera_status = NatilleraStatus.STARTED;
     }
-    //Requiered verification
-    //60% thresold
-    //Memory copia temporal
-    //storage address real
-    function Withdraw(
-        uint256 id,
-        uint256 amount,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    ) external Natillera_Status Member_Status(id) {
-        MemberData memory member = s_members_id[id];
-        if (member.addr != msg.sender) {
-            revert Wallet__SpenderNotValid(msg.sender);
-        }
-        bytes32 digest = _getMessageHash(member.addr, amount);
-        if (!_isValidSignature(digest, _v, _r, _s)) {
-            revert Wallet_SignatureInvalid();
-        }
-        s_nonce++;
-    }
+
     function period() internal view returns (uint256) {
         return (block.timestamp - s_time) / (30 days);
     }
@@ -393,64 +360,16 @@ emergencyWithdraw()
     ///////////////////////////////
     ////// INTERNAL FUNCTIONS //////
     ///////////////////////////////
-    /**
-     * @dev This function returns the message hash for the claim_Check function.
-     * @param account The address to transfer the funds to.
-     * @param amount The amount to transfer.
-     * @param token The address of the token to transfer.
-     * @return The message hash.
-     */
 
-    function _getMessageHash(
-        address account,
-        uint256 amount,
-        address token
-    ) internal view returns (bytes32) {
-        return
-            _hashTypedDataV4(
-                keccak256(
-                    abi.encode(
-                        MESSAGE_TYPEHASH,
-                        CheckClaim({
-                            spender: account,
-                            value: amount,
-                            nonce: s_nonce,
-                            deadline: block.timestamp + 30 days
-                        })
-                    )
-                )
-            );
-    }
-
-    /**
-     * @dev This function checks if the signature is valid.
-     * @param digest The message hash.
-     * @param _v The recovery byte of the signature.
-     * @param _r The r value of the signature.
-     * @param _s The s value of the signature.
-     * @return True if the signature is valid, false otherwise.
-     */
-    function _isValidSignature(
-        bytes32 digest,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    ) internal view returns (bool) {
-        // could also use SignatureChecker.isValidSignatureNow(signer, digest, signature)
-        (
-            address actualSigner /*ECDSA.RecoverError recoverError*/ /*bytes32 signatureLength*/,
-            ,
-
-        ) = ECDSA.tryRecover(digest, _v, _r, _s);
-        return (actualSigner == i_owner);
-    }
-    function is_myTurn(uint256 id) internal Member_Status(id) returns (bool) {
+    function is_myTurn(
+        uint256 id
+    ) internal view Member_Status(id) returns (bool) {
         MemberData memory member = s_members_id[id];
-        uint64 turn = s_members_turn[id];
+        uint256 turn = s_members_turn[id];
         if (member.claim == true) {
             revert Natillera_Member_already_claim(msg.sender);
         }
-        uint256 memory s_period = period();
+        uint256 s_period = period();
         bool ismyturn = false;
         if (s_period < (s_periods_claim * turn)) {
             ismyturn = true;
@@ -460,19 +379,39 @@ emergencyWithdraw()
     //////////////////////////
     ////// VIEW FUNCTIONS ////
     //////////////////////////
-    /**
-     * @dev This function returns the message hash for the claim_Check function.
-     * @param account The address to transfer the funds to.
-     * @param amount The amount to transfer.
-     * @param token The address of the token to transfer.
-     * @return The message hash.
-     * @dev The function is view, so it does not modify the state of the contract.
-     */
-    function getMessageHash(
-        address account,
-        uint256 amount,
-        address token
-    ) external view returns (bytes32) {
-        return _getMessageHash(account, amount, token);
+    function getData()
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint16,
+            uint256,
+            uint256,
+            address,
+            uint256,
+            uint256[] memory
+        )
+    {
+        return (
+            s_total_member,
+            s_amount_late,
+            s_periods_claim,
+            s_amount,
+            s_time,
+            s_moneyAddr,
+            uint256(s_natillera_status),
+            membersId
+        );
     }
+    function getdataMember(
+        uint256 id
+    ) external view returns (MemberData memory, uint256) {
+        MemberData memory member = s_members_id[id];
+        uint256 turn = s_members_turn[id];
+        return (member, turn);
+    }
+    /**
+    
+     */
 }
